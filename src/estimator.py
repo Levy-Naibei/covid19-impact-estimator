9d4be1160f389644360454667f2bf8322282136f
Levy-Naibei <levynaibei2@gmail.com>
2020-04-12 19:33:42 +0300

    fixed challenge 1 2

--- src/estimator.py
+++ src/estimator.py
@@ -1,43 +1,49 @@
 def estimator(data):
   impact = {}  # your best case estimation
   severe_impact = {}  # your severe case estimation
-  # import pdb; pdb.set_trace()
-  total_hospital_beds = (0.35 * data['totalHospitalBeds'])
-  time_to_elapse = (2 ** (data['timeToElapse'] / 3))
 
+  if data['periodType'] == 'days':
+      time_to_elapse = data['timeToElapse']
+  elif data['periodType'] == 'weeks':
+      time_to_elapse = data['timeToElapse'] * 7
+  elif data['periodType'] == 'months':
+      time_to_elapse = data['timeToElapse'] * 30
+
+  time_to_elapse = 2 ** (int(time_to_elapse/ 3))
+  total_hospital_beds = int(0.35 * data['totalHospitalBeds'])
+
+  # challenge-1
   # currentlyInfected
-  impact['currentlyInfected'] = (data['reportedCases'] * 10)
-  severe_impact['currentlyInfected'] = (data['reportedCases'] * 50)
+  impact['currentlyInfected'] = int(data['reportedCases'] * 10)
+  severe_impact['currentlyInfected'] = int(data['reportedCases'] * 50)
 
   # infectionsByRequestedTime
-  impact['infectionsByRequestedTime'] = (impact['currentlyInfected'] * time_to_elapse)
-  severe_impact['infectionsByRequestedTime'] = (severe_impact['currentlyInfected'] * time_to_elapse)
+  impact['infectionsByRequestedTime'] = int(impact['currentlyInfected'] * time_to_elapse)
+  severe_impact['infectionsByRequestedTime'] = int(severe_impact['currentlyInfected'] * time_to_elapse)
 
-  # # severeCasesByRequestedTime
-  # impact['severeCasesByRequestedTime'] = (0.15 * impact['infectionsByRequestedTime'])
-  # severe_impact['severeCasesByRequestedTime'] = (0.15 * severe_impact[
-  #                                                 'infectionsByRequestedTime'])
-  #
-  # # total_hospital_beds
-  # impact['hospitalBedsByRequestedTime'] = total_hospital_beds - impact[
-  #                                         'severeCasesByRequestedTime']
-  # severe_impact['hospitalBedsByRequestedTime'] = total_hospital_beds - severe_impact[
-  #                                                 'severeCasesByRequestedTime']
-  #
+  # challenge-2
+  # severeCasesByRequestedTime
+  impact['severeCasesByRequestedTime'] = int(0.15 * impact['infectionsByRequestedTime'])
+  severe_impact['severeCasesByRequestedTime'] = int(0.15 * severe_impact['infectionsByRequestedTime'])
+
+  # total_hospital_beds
+  impact['hospitalBedsByRequestedTime'] = int(total_hospital_beds - impact['severeCasesByRequestedTime'])
+  severe_impact['hospitalBedsByRequestedTime'] = int(total_hospital_beds - severe_impact['severeCasesByRequestedTime'])
+
+  # # challenge-3
   # # casesForICUByRequestedTime
-  # impact['casesForICUByRequestedTime'] = (0.05 * impact['infectionsByRequestedTime'])
-  # severe_impact['casesForICUByRequestedTime'] = (0.05 * severe_impact[
-  #                                                'infectionsByRequestedTime'])
+  # impact['casesForICUByRequestedTime'] = int(0.05 * impact['infectionsByRequestedTime'])
+  # severe_impact['casesForICUByRequestedTime'] = int(0.05 * severe_impact['infectionsByRequestedTime'])
   #
   # # casesForVentilatorsByRequestedTime
-  # impact['casesForVentilatorsByRequestedTime'] = (0.02 * impact['infectionsByRequestedTime'])
-  # severe_impact['casesForVentilatorsByRequestedTime'] = (0.02 * severe_impact[
-  #                                                        'infectionsByRequestedTime'])
+  # impact['casesForVentilatorsByRequestedTime'] = int(0.02 * impact['infectionsByRequestedTime'])
+  # severe_impact['casesForVentilatorsByRequestedTime'] = int(0.02 * severe_impact['infectionsByRequestedTime'])
   #
   # # dollarsInFlight
-  # impact['dollarsInFlight'] = ((impact['infectionsByRequestedTime'] * 0.65 * 1.5) / 30)
-  # severe_impact['dollarsInFlight'] = ((severe_impact[
-  #                                        'infectionsByRequestedTime'] * 0.65 * 1.5) / 30)
+  # impact['dollarsInFlight'] = int((impact['infectionsByRequestedTime']
+  #                                  * 0.65 * 1.5) / time_to_elapse)
+  # severe_impact['dollarsInFlight'] = int((severe_impact['infectionsByRequestedTime']
+  #                                         * 0.65 * 1.5) / time_to_elapse)
 
   output = {
     "data": data,
