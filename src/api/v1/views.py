f6fe08da6fbc933544d2f3c3d9ff5830979b02ff
Levy-Naibei <levynaibei2@gmail.com>
2020-04-13 23:47:43 +0300

    added procfile n requirements

--- src/api/v1/views.py
+++ src/api/v1/views.py
@@ -1,21 +1,21 @@
 """Novel covid-19 impact estimator flask REST API"""
 
 from flask import Flask, request, Response
-from ..helpers.estimator_helper import estimator_helper
+from src.estimator import estimator
 import dicttoxml
 
 app = Flask(__name__)
 
 @app.route('/api/v1/on-covid-19', methods=['POST'])
-def estimator():
+def estimator_json():
     data = request.get_json()
-    output = estimator_helper(data)
+    output = estimator(data)
     return output
 
 @app.route('/api/v1/on-covid-19/xml', methods=['POST'])
 def estimator_xml():
 
     data = request.get_json()
-    output = estimator_helper(data)
+    output = estimator(data)
     xml = dicttoxml.dicttoxml(output)
     return Response(xml, mimetype='text/xml')
