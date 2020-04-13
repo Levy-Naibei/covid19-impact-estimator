13ba2e9690798a7e06ef3eb27202101adf1c0093
Levy-Naibei <levynaibei2@gmail.com>
2020-04-13 22:48:44 +0300

    xml

--- src/api/v1/views.py
+++ src/api/v1/views.py
@@ -1,7 +1,8 @@
 """Novel covid-19 impact estimator flask REST API"""
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from ..helpers.estimator_helper import estimator_helper
+import dicttoxml
 
 app = Flask(__name__)
 
@@ -10,3 +11,11 @@ def estimator():
     data = request.get_json()
     output = estimator_helper(data)
     return output
+
+@app.route('/api/v1/on-covid-19/xml', methods=['POST'])
+def estimator_xml():
+
+    data = request.get_json()
+    output = estimator_helper(data)
+    xml = dicttoxml.dicttoxml(output)
+    return Response(xml, mimetype='text/xml')
