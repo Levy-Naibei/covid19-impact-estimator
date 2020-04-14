763bd16145bb6fabbe4e8460be97ad494baebdd2
Levy-Naibei <levynaibei2@gmail.com>
2020-04-14 12:20:11 +0300

    response data

--- src/api/v1/views.py
+++ src/api/v1/views.py
@@ -1,22 +1,45 @@
 """Novel covid-19 impact estimator flask REST API"""
 
 import dicttoxml
-from flask import Flask, request, Response
+import os
+import time
+import logging
+from flask import Flask, request, Response, jsonify
 from src.estimator import estimator
+
+# check if logs.json file already exists
+# if 'logs.json' in os.listdir():
+#     os.remove('logs.json')
 
 app = Flask(__name__)
 
-@app.route('/api/v1/on-covid-19', methods=['POST'])
+# initializes file to persist logs
+# logging.BasicConfig(filename = 'logs.json', level=logging.INFO)
+
+
+@app.route('/api/v1/on-covid-19', methods=['GET', 'POST'])
+@app.route('/api/v1/on-covid-19/json', methods=['GET', 'POST'])
 def estimator_json():
     """handles responses in json format"""
-    data = request.get_json()
-    output = estimator(data)
-    return output
+    if request.method == 'GET':
+        response = Response("", mimetype='application/json')
+        return response, 200
 
-@app.route('/api/v1/on-covid-19/xml', methods=['POST'])
+    if request.method == 'POST':
+        data = request.get_json()
+        json_data = estimator(data)
+        return jsonify(json_data), 200
+
+@app.route('/api/v1/on-covid-19/xml', methods=['GET', 'POST'])
 def estimator_xml():
     """handles responses in xml format"""
-    data = request.get_json()
-    output = estimator(data)
-    xml = dicttoxml.dicttoxml(output)
-    return Response(xml, mimetype='text/xml')
+
+    if request.method == 'GET':
+        response = Response("", mimetype='text/xml')
+        return response, 200
+
+    if request.method == 'POST':
+        data = request.get_json()
+        output = estimator(data)
+        xml = dicttoxml.dicttoxml(output)
+        return Response(xml, mimetype='text/xml')
